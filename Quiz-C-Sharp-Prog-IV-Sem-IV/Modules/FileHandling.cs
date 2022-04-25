b5a90e111e75a81393a21b8e35372f3bd1476dd3
Grzegorz Jaworski <35576532+jaworskig00@users.noreply.github.com>
2022-04-25 15:28:42 +0200

    feat/quiz-view-functionality
    * Added navigation between views functionality
    * Added all modules:
    - Quiz strructure
    - FileHandling class
    - Cezar Encryption

---
+++ Quiz-C-Sharp-Prog-IV-Sem-IV/Modules/FileHandling.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+
+namespace QuizGenerator_programowanie_IV.Modules
+{
+    class FileHandling
+    {
+        public List<Quiz> quizzes = new List<Quiz>();
+        public void SaveToFile(Quiz quiz)
+        {
+            //string json = JsonSerializer.Serialize(quiz);
+
+            // tutaj będzie szyfrowanie
+
+            // można tutaj dodać jak nam się będzie nudziło żeby program tworzył nowy folder w "Dokumentach" użytkownika i tam zapisywał quiz'y
+            File.WriteAllText($"C:\\Users\\2000g\\OneDrive\\Pulpit\\QUIZY\\{quiz.QuizName}.txt", json);
+        }
+
+        public List<Quiz> ReadFromFile()
+        {
+
+            quizzes.Clear();
+            string[] files = Directory.GetFiles("C:\\Users\\2000g\\OneDrive\\Pulpit\\Quizy\\");
+
+            foreach (string file in files)
+            {
+                string text = File.ReadAllText(file);
+                //Quiz stuff = JsonSerializer.Deserialize<Quiz>(text);
+
+                quizzes.Add(stuff);
+            }
+
+            return quizzes;
+        }
+    }
+}
---
+++ Quiz-C-Sharp-Prog-IV-Sem-IV/Modules/Quiz.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizGenerator_programowanie_IV.Modules
+{
+    class Quiz
+    {
+        public string QuizName { get; set; }
+        public List<Question> Questions { get; set; }
+
+        public Quiz(string quizName, List<Question> questions)
+        {
+            this.QuizName = quizName;
+            this.Questions = new List<Question>();
+
+            foreach (Question question in questions)
+            {
+                this.Questions.Add(question);
+            }
+        }
+    }
+}
