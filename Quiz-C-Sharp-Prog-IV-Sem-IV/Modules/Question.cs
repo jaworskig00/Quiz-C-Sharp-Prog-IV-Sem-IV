b8b8225874017a1256a0575271483ed72feed050
jaworskig00 <2000grzegorz@gmail.com>
2022-04-25 08:30:31 +0200

    Added all modules: - Quiz strructure - FileHandling class - Cezar Encryption

---
+++ Quiz-C-Sharp-Prog-IV-Sem-IV/Modules/Question.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizGenerator_programowanie_IV.Modules
+{
+    class Question
+    {
+        public int QuestionNumber { get; set; }
+        public string QuestionText { get; set; }
+        public int Time { get; set; }
+        public List<Answer> Answers { get; set; }
+
+        public Question(int questionNumber, string questionText, int time, List<Answer> answers)
+        {
+            this.QuestionNumber = questionNumber;
+            this.QuestionText = questionText;
+            this.Time = time;
+            this.Answers = new List<Answer>();
+
+            foreach (Answer answer in answers)
+            {
+                this.Answers.Add(answer);
+            }
+        }
+    }
+}
